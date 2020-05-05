92947c6b012b7e0562fb4350c82deec1d8938f59
Andrew <andrew.panix@gmail.com>
2020-05-05 16:36:04 +0300

    first commit

---
+++ Module.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace panix\mod\pages;
+
+use Yii;
+use panix\engine\WebModule;
+use yii\base\BootstrapInterface;
+
+class Module extends WebModule implements BootstrapInterface
+{
+
+    public $icon = 'edit';
+
+    public function bootstrap($app)
+    {
+        $app->urlManager->addRules(
+            [
+                'page/<slug:[0-9a-zA-Z_\-]+>/page/<page:\d+>/per-page/<per-page:\d+>' => 'pages/default/view',
+                'page/<slug:[0-9a-zA-Z_\-]+>/page/<page:\d+>' => 'pages/default/view',
+                'page/<slug:[0-9a-zA-Z_\-]+>' => 'pages/default/view',
+
+            ],
+            true
+        );
+    }
+
+    public function getAdminMenu()
+    {
+        return [
+            'modules' => [
+                'items' => [
+                    [
+                        'label' => Yii::t('pages/default', 'MODULE_NAME'),
+                        'url' => ['/admin/pages'],
+                        'icon' => $this->icon,
+                    ],
+                ],
+            ],
+        ];
+    }
+
+
+    public function getInfo()
+    {
+        return [
+            'label' => Yii::t('pages/default', 'MODULE_NAME'),
+            'author' => 'dev@pixelion.com.ua',
+            'version' => '1.0',
+            'icon' => $this->icon,
+            'description' => Yii::t('pages/default', 'MODULE_DESC'),
+            'url' => ['/admin/pages'],
+        ];
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+# mod-pages
+
+Module for PIXELION CMS
+
+[![Latest Stable Version](https://poser.pugx.org/panix/mod-pages/v/stable)](https://packagist.org/packages/panix/mod-pages) [![Total Downloads](https://poser.pugx.org/panix/mod-pages/downloads)](https://packagist.org/packages/panix/mod-pages) [![Monthly Downloads](https://poser.pugx.org/panix/mod-pages/d/monthly)](https://packagist.org/packages/panix/mod-pages) [![Daily Downloads](https://poser.pugx.org/panix/mod-pages/d/daily)](https://packagist.org/packages/panix/mod-pages) [![Latest Unstable Version](https://poser.pugx.org/panix/mod-pages/v/unstable)](https://packagist.org/packages/panix/mod-pages) [![License](https://poser.pugx.org/panix/mod-pages/license)](https://packagist.org/packages/panix/mod-pages)
+
+
+## Installation
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+#### Either run
+
+```
+php composer require --prefer-dist panix/mod-pages "*"
+```
+
+or add
+
+```
+"panix/mod-pages": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+#### Add to web config.
+```
+    'modules' => [
+        'pages' => ['class' => 'panix\mod\pages\Module'],
+    ],
+```
+#### Migrate
+```
+php yii migrate --migrationPath=vendor/panix/mod-pages/migrations
+```
