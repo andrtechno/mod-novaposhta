b7e709b8bc205086801cd3d76a41e613cdf60f3a
Andrew <andrew.panix@gmail.com>
2020-05-05 16:43:52 +0300

    init

---
+++ controllers/DefaultController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace panix\mod\novaposhta\controllers;
+
+use Yii;
+use panix\engine\controllers\WebController;
+use panix\mod\pages\models\Pages;
+use yii\helpers\ArrayHelper;
+use yii\web\View;
+
+class DefaultController extends WebController
+{
+    public function behaviors1()
+    {
+        $behaviors[] = [
+            'class' => 'yii\filters\PageCache',
+            'only' => ['view'],
+            'duration' => 86400 * 30,
+            'variations' => [
+                //Yii::$app->language,
+                Yii::$app->request->get('slug')
+            ],
+            'dependency' => [
+                'class' => 'yii\caching\DbDependency',
+                'sql' => 'SELECT MAX(updated_at) FROM ' . Pages::tableName(),
+            ]
+        ];
+        return ArrayHelper::merge(parent::behaviors(), $behaviors);
+    }
+
+    public function actionView($slug)
+    {
+        $layouts = [
+            "@theme/modules/pages/views/default/html",
+            "@pages/views/default/html",
+        ];
+
+        foreach ($layouts as $layout) {
+            if (file_exists(Yii::getAlias($layout) . DIRECTORY_SEPARATOR . $slug . '.' . $this->view->defaultExtension)) {
+                return $this->render($layout . '/' . $slug, []);
+            }
+        }
+
+        $this->dataModel = Pages::find()
+            ->where(['slug' => $slug])
+            ->published()
+           // ->cache(3200, new \yii\caching\DbDependency(['sql' => 'SELECT MAX(updated_at) FROM ' . Pages::tableName()]))
+            ->one();
+
+        if (!$this->dataModel) {
+            $this->error404();
+        }
+        $this->pageName = $this->dataModel->name;
+        $this->breadcrumbs = [$this->pageName];
+
+        $this->view->setModel($this->dataModel);
+
+
+
+        $this->view->title = $this->pageName;
+        return $this->render('view', ['model' => $this->dataModel]);
+    }
+
+}
---
+++ migrations/m170908_125123_novaposhta.php
@@ -0,0 +1,80 @@
+<?php
+namespace panix\mod\novaposhta\migrations;
+
+/**
+ * Generation migrate by PIXELION CMS
+ * @author PIXELION CMS development team <dev@pixelion.com.ua>
+ *
+ * Class m170908_125123_novaposhta
+ */
+use Yii;
+use yii\db\Migration;
+use panix\mod\pages\models\Pages;
+use panix\mod\pages\models\PagesTranslate;
+class m170908_125123_novaposhta extends Migration
+{
+
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable(Pages::tableName(), [
+            'id' => $this->primaryKey()->unsigned(),
+            'user_id' => $this->integer()->unsigned(),
+            'slug' => $this->string(255)->notNull(),
+            'views' => $this->integer()->defaultValue(0),
+            'ordern' => $this->integer()->unsigned(),
+            'switch' => $this->boolean()->defaultValue(1),
+            'created_at' => $this->integer(11)->null(),
+            'updated_at' => $this->integer(11)->null()
+        ], $tableOptions);
+
+
+        $this->createTable(PagesTranslate::tableName(), [
+            'id' => $this->primaryKey()->unsigned(),
+            'object_id' => $this->integer()->unsigned(),
+            'language_id' => $this->tinyInteger()->unsigned(),
+            'name' => $this->string(255),
+            'text' => $this->text()
+        ], $tableOptions);
+
+
+        $this->createIndex('switch', Pages::tableName(), 'switch');
+        $this->createIndex('ordern', Pages::tableName(), 'ordern');
+        $this->createIndex('user_id', Pages::tableName(), 'user_id');
+        $this->createIndex('slug', Pages::tableName(), 'slug');
+
+        $this->createIndex('object_id', PagesTranslate::tableName(), 'object_id');
+        $this->createIndex('language_id', PagesTranslate::tableName(), 'language_id');
+
+        if ($this->db->driverName != "sqlite") {
+            $this->addForeignKey('{{%fk_pages_translate}}', PagesTranslate::tableName(), 'object_id', Pages::tableName(), 'id', "CASCADE", "NO ACTION");
+        }
+
+        $columns = ['slug', 'user_id', 'ordern', 'created_at'];
+        $this->batchInsert(Pages::tableName(), $columns, [
+            ['about', 1, 1, time()],
+            ['mypage', 1, 2, time()],
+        ]);
+
+
+        $columns = ['object_id', 'language_id', 'name', 'text'];
+        $this->batchInsert(PagesTranslate::tableName(), $columns, [
+            [1, 1, 'О компании', ''],
+            [2, 1, 'Тест', ''],
+        ]);
+    }
+
+    public function down()
+    {
+        if ($this->db->driverName != "sqlite") {
+            $this->dropForeignKey('{{%fk_pages_translate}}', PagesTranslate::tableName());
+        }
+        $this->dropTable(Pages::tableName());
+        $this->dropTable(PagesTranslate::tableName());
+    }
+
+}
